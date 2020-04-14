96832107ce9b2b541809bbb2c26fe424cb160693
mathiasfc <mathias.falci@gmail.com>
2020-04-14 20:14:07 -0300

    :fire:

--- vue/my-project/src/service/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default () => {
-  return axios.create({
-    baseURL: (process.env.NODE_ENV === 'production') ? `` : `http://localhost:3000/`,
-    withCredentials: true
-  })
-}
